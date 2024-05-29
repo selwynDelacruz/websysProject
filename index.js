f170422318b4f105367aed3a179b3411393a9b15
SeseGuru <134850743+d4rky19@users.noreply.github.com>
2024-05-29 10:51:29 +0800

    eto pre

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ function showSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'flex';
 }
+        
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll(".carousel-image");
+    let currentIndex = 0;
+
+    function showNextImage() {
+        images[currentIndex].classList.remove("active");
+        currentIndex = (currentIndex + 1) % images.length;
+        images[currentIndex].classList.add("active");
+    }
+
+    images[currentIndex].classList.add("active");
+    setInterval(showNextImage, 3000); // Change image every 3 seconds
+});
+
+const data = [{id: 5, name: 'photocard1'}]
